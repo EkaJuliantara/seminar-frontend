dc2427d8f09b80a8ac5eca161359d7dccef1c8f4
Michael Alan <aran.arukado@outlook.com>
2017-03-19 06:15:06 +0700

    fixed bug menu

--- js/animation.js
+++ js/animation.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
   $(window).scroll(function(){
-    var shrinkOn=$("#header").height(), paddingAdd=$("#header").height()-2;
-    if($(window).scrollTop()>shrinkOn+11)
+    var shrinkOn=$("#header").height(), paddingAdd=$("#header").height()-3;
+    if($(window).scrollTop()>shrinkOn+9)
     {
       $("#menu").css({'position':'fixed'});
       $("#theme").css({'margin-top':paddingAdd+'px'});
       $("#form-header").css({'margin-top':paddingAdd+'px'});
+      $("#logo").css({'margin-top':paddingAdd+'px'});
     }
     else
     {
       $("#menu").css({'position':'static'});
       $("#theme").css({'margin-top':'0px'});
       $("#form-header").css({'margin-top':'0px'});
+      $("#logo").css({'margin-top':'0px'});
     }
   });
 
